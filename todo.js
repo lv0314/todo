c3bdf090f3dd3865b36d8e9b84ee80ea19574569
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2021-12-15 17:25:54 +0900

    ver1 fin

--- todo.js
+++ todo.js
@@ -1,29 +1,75 @@
 const btn = document.getElementById("btn")
 const input = document.getElementById("input")
+const listspace = document.getElementById("listspace")
 const ul = document.getElementById("ul")
-
-function addList(){
-    let inputText = document.getElementById("input").value
-    var list = document.createElement("li");
-    list.innerHTML = inputText
-    ul.appendChild(list)
-    init();
-}
 
 function init(){
     input.value = '';
 }
 
+function addList(){
+    let inputText = document.getElementById("input").value
+    if (inputText != ""){
+        let list = document.createElement("div");
+        list.setAttribute("id", "content")
+        list.innerHTML = inputText
+        init();
+        return list;
+    }
+}
+
+function addCheckBtn(){
+    let checkBtn = document.createElement("button")
+    checkBtn.setAttribute("id", "check")
+    checkBtn.addEventListener("click", checkFunc)
+    return checkBtn;
+}
+
+function addDelBtn(){
+    let delBtn = document.createElement("button")
+    delBtn.innerHTML = "del";
+    delBtn.setAttribute("id", "del")
+    delBtn.addEventListener("click", delFunc)
+    return delBtn;
+}
+
+function combi(){
+    let combiSpace = document.createElement("div")
+    combiSpace.setAttribute("id", "combi")
+
+    combiSpace.appendChild(addList())
+    combiSpace.appendChild(addCheckBtn())
+    combiSpace.appendChild(addDelBtn())
+    listspace.appendChild(combiSpace)
+}
+
 function addBtnOp(){
-    btn.addEventListener("click", addList)
+    btn.addEventListener("click", combi)
 }
 
 function addInputOp(){
     input.addEventListener("keydown", function(t){
         if (t.key == "Enter"){
-            addList();
+            combi();
         }
     })
+}
+
+let check = 1;
+function checkFunc(){
+    if(check % 2 == 1){
+        this.style.backgroundColor = "#a3d390"
+        check++;
+        this.previousSibling.style.textDecoration = "line-through";
+    } else if (check % 2 == 0) {
+        this.style.backgroundColor = "lightgray"
+        check--;
+        this.previousSibling.style.textDecoration = "none"
+    }
+}
+
+function delFunc(){
+    this.parentNode.remove();
 }
 
 function main(){
@@ -32,3 +78,4 @@ function main(){
 }
 
 main();
+
